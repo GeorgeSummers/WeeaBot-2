b6974d049dd5c72bbd884207f272c15c40107974
GeorgeSummers <tankist1121@gmail.com>
2020-12-12 00:24:43 +0300

    Config'n'classes

--- src/main/scala/WeeaBot/VkHandler.scala
+++ src/main/scala/WeeaBot/VkHandler.scala
@@ -1,9 +1,41 @@
 package WeeaBot
 
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.{ExecutorService, Executors}
+
+import com.typesafe.config.{Config, ConfigFactory}
+import com.vk.api.sdk.exceptions.{ApiException, ClientException}
+import com.vk.api.sdk.objects.messages._
+import org.apache.logging.log4j.core.pattern.AbstractStyleNameConverter.White
+
 object VkHandler {
+
+  @throws(classOf[NullPointerException])
+  @throws(classOf[ApiException])
+  @throws(classOf[InterruptedException])
   def main(args: Array[String]): Unit = {
-    VkEngine.instance(117602761,
-    "f5612149dbfbd9acd784813709d157219f7ae021146a593b1997f51fc1d5ce3c3a8b37b5aad387df49581")
+    println("Running...")
+    val confFileName = "cred.conf"
+    val config = ConfigFactory.load(confFileName)
+    val groupId: Int = config.getInt("groupId")
+    val accessToken: String = config.getString("access_token")
+    VkEngine.instance(groupId, accessToken)
+    while (true) {
+      Thread.sleep(300)
+      try {
+        val message: Message = VkEngine.getMessage
+        if (message != null) {
+          val exec: ExecutorService = Executors.newCachedThreadPool()
+          exec.execute(new Messenger(message))
+        }
+      } catch {
+        case e: ClientException =>
+          val rct: Int = 10000
+          println(e.getMessage)
+          Thread.sleep(rct)
+      }
+    }
   }
 
 }
